376a7bab484d57e67bd450ddc2b5f17c12e22481
spoyodevelop <kjy9505@gmail.com>
2025-02-24 16:08:18 +0900

    feat: prizeMoney를 참조하여, results에 금액을 얻어옴

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
 import makeLotto from './src/service/LottoService.js';
-import validateLottoPurchase from './src/Validation/validateLottoPurchase';
+import validateLottoPurchase from './src/Validation/validateLottoPurchase.js';
 import Lotto from './src/model/Lotto.js';
 import validateBonusNumber from './src/Validation/validateBonusNumber.js';
+import {
+  calculatePrize,
+  calculateRevenueRate,
+  calculateWins,
+} from './src/service/CalculatorService';
+import { lottoResults } from './src/settings/systemSettings.js';
+
 let lottos;
+let purchasePrice = 0;
+
+document.addEventListener('DOMContentLoaded', () => {
+  const idMapping = {
+    THREE_MATCH: 'threeMatchPrice',
+    FOUR_MATCH: 'fourMatchPrice',
+    FIVE_MATCH: 'fiveMatchPrice',
+    FIVE_MATCH_WITH_BONUS: 'fiveMatchWithBonusPrice',
+    SIX_MATCH: 'sixMatchPrice',
+  };
+
+  Object.entries(idMapping).forEach(([key, id]) => {
+    const element = document.getElementById(id);
+    if (element) {
+      element.innerHTML = lottoResults.prizeMoney[key].toLocaleString(); // 천 단위 콤마 추가
+    }
+  });
+});
 
 document.addEventListener('DOMContentLoaded', () => {
   const userMoneyInput = document.getElementById('userMoney');
@@ -13,10 +38,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
   purchaseLottoButton.addEventListener('click', () => {
     const inputValue = userMoneyInput.value.trim();
+
     try {
       const ticket = validateLottoPurchase(inputValue);
+      purchasePrice = +inputValue;
       lottos = makeLotto(ticket, 'web');
       showLottoList(lottos);
+
       lottosDiv.classList.remove('hidden');
       checkUserNumberDiv.classList.remove('hidden');
       userMoneyInput.value = '';
@@ -41,19 +69,20 @@ document.addEventListener('DOMContentLoaded', () => {
         fifthNumber,
         sixthNumber,
       ]);
-      const { checkedlotto, checkedBonusNumber } = validateBonusNumber(
-        userLotto,
-        bonusNumber,
-      );
+      const parsedLotto = validateBonusNumber(userLotto, bonusNumber);
+
+      const winCount = calculateWins(lottos, parsedLotto);
+      const total = calculatePrize(winCount, lottoResults.prizeMoney);
+      const revenueRate = calculateRevenueRate(total, purchasePrice);
+
+      calculatePrize();
+      calculateRevenueRate();
     } catch (error) {
       alert(error.message);
     }
   });
 });
 
-document.addEventListener('DOMContentLoaded', () => {
-  const button = document.getElementById('myButton');
-});
 function showLottoList(lottos) {
   const lottoList = document.getElementById('lottoList');
 
