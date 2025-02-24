0402d7f691ab42d9846726639da37cbb80cec93e
spoyodevelop <kjy9505@gmail.com>
2025-02-24 22:49:07 +0900

    refactor(GameService): 게임 관련한 로직을 조종하는 것을 GameService로 분리

---
+++ src/service/GameService.js
@@ -0,0 +1,92 @@
+import gameState from '../state/State.js';
+import { elements } from '../View/elements.js';
+import makeLotto from './LottoService.js';
+import validateLottoPurchase from '../Validation/validateLottoPurchase.js';
+import validateBonusNumber from '../Validation/validateBonusNumber.js';
+import Lotto from '../model/Lotto.js';
+import { showLottoList } from '../View/LottoView.js';
+import { resetInputs } from '../View/LottoView.js';
+import {
+  calculatePrize,
+  calculateRevenueRate,
+  calculateWins,
+} from './CalculatorService.js';
+import { lottoResults } from '../settings/systemSettings';
+import {
+  showRevenueRate,
+  updateWinCount,
+  resetLottoList,
+} from '../View/LottoView.js';
+
+function handlePurchaseLotto() {
+  const inputValue = elements.userMoneyInput.value.trim();
+  try {
+    const ticket = validateLottoPurchase(inputValue);
+    gameState.setPurchasePrice(+inputValue);
+    gameState.setLottos(makeLotto(ticket, 'web'));
+    showLottoList(gameState.lottos);
+    elements.purchaseAmount.innerHTML = `총 ${ticket}개를 구매하였습니다.`;
+    elements.lottosDiv.classList.remove('hidden');
+    elements.checkUserNumberDiv.classList.remove('hidden');
+    elements.userMoneyInput.value = '';
+  } catch (error) {
+    alert(error.message);
+  }
+}
+
+function handleCheckResult() {
+  try {
+    const inputIds = [
+      'first-number',
+      'second-number',
+      'third-number',
+      'fourth-number',
+      'fifth-number',
+      'sixth-number',
+    ];
+    const userNumbers = inputIds.map((id) => document.getElementById(id).value);
+    const bonusNumber = document.getElementById('bonus-number').value;
+
+    const userLotto = new Lotto(userNumbers);
+    resetInputs([...inputIds, 'bonus-number']);
+
+    const parsedLotto = validateBonusNumber(userLotto, bonusNumber);
+    const winCount = calculateWins(gameState.lottos, parsedLotto);
+    const totalPrize = calculatePrize(winCount, lottoResults.prizeMoney);
+    const revenueRate = calculateRevenueRate(
+      totalPrize,
+      gameState.purchasePrice,
+    );
+
+    showRevenueRate(revenueRate);
+    updateWinCount(winCount);
+    elements.resultModal.classList.remove('hidden');
+  } catch (error) {
+    alert(error.message);
+  }
+}
+function retryGame() {
+  gameState.resetGameState();
+  resetInputs([
+    'first-number',
+    'second-number',
+    'third-number',
+    'fourth-number',
+    'fifth-number',
+    'sixth-number',
+    'bonus-number',
+  ]);
+  resetLottoList();
+  updateWinCount({
+    THREE_MATCH: 0,
+    FOUR_MATCH: 0,
+    FIVE_MATCH: 0,
+    FIVE_MATCH_WITH_BONUS: 0,
+    SIX_MATCH: 0,
+  });
+  elements.resultModal.classList.add('hidden');
+  elements.checkUserNumberDiv.classList.add('hidden');
+  elements.lottosDiv.classList.add('hidden');
+}
+
+export { handleCheckResult, handlePurchaseLotto, retryGame };
