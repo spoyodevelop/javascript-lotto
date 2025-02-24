a45fe263008abe82d902579540e3fa144c8c691c
spoyodevelop <kjy9505@gmail.com>
2025-02-24 16:55:15 +0900

    fix(validateBonusNumber): bonusNumber는 숫자를 반환함

--- src/Validation/validateBonusNumber.js
+++ src/Validation/validateBonusNumber.js
@@ -7,5 +7,5 @@ export default function validateBonusNumber(lotto, bonusNumber) {
   validateNumberInRange([bonusNumber]);
   if (lotto.numbers.includes(bonusNumber))
     throw new Error(ERROR_MESSAGE.duplicatedBonusNumbers);
-  return { checkedLotto: lotto, checkedBonusNumber: bonusNumber };
+  return { checkedLotto: lotto, checkedBonusNumber: Number(bonusNumber) };
 }
