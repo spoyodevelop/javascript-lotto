ac83ee6a436d3091f44c71fdc76ab56e4a4ca4cb
spoyodevelop <kjy9505@gmail.com>
2025-02-24 15:33:23 +0900

    feat(checkResultButton): 로또 번호 validation을 적용

--- script.js
+++ script.js
@@ -1,29 +1,59 @@
 import makeLotto from './src/service/LottoService.js';
-
 import validateLottoPurchase from './src/Validation/validateLottoPurchase';
-
+import Lotto from './src/model/Lotto.js';
+import validateBonusNumber from './src/Validation/validateBonusNumber.js';
 let lottos;
 
 document.addEventListener('DOMContentLoaded', () => {
-  const input = document.getElementById('userInput');
-  const button = document.getElementById('myButton');
+  const userMoneyInput = document.getElementById('userMoney');
+  const purchaseLottoButton = document.getElementById('purchaseLotto');
   const lottosDiv = document.getElementById('lottos');
   const checkUserNumberDiv = document.getElementById('checkUserNumber');
+  const checkResultButton = document.getElementById('checkResult');
 
-  button.addEventListener('click', async () => {
-    const inputValue = input.value.trim();
+  purchaseLottoButton.addEventListener('click', () => {
+    const inputValue = userMoneyInput.value.trim();
     try {
       const ticket = validateLottoPurchase(inputValue);
       lottos = makeLotto(ticket, 'web');
       showLottoList(lottos);
       lottosDiv.classList.remove('hidden');
       checkUserNumberDiv.classList.remove('hidden');
+      userMoneyInput.value = '';
+    } catch (error) {
+      alert(error.message);
+    }
+  });
+  checkResultButton.addEventListener('click', () => {
+    const firstNumber = document.getElementById('firstNumber').value;
+    const secondNumber = document.getElementById('secondNumber').value;
+    const thirdNumber = document.getElementById('thirdNumber').value;
+    const fourthNumber = document.getElementById('fourthNumber').value;
+    const fifthNumber = document.getElementById('fifthNumber').value;
+    const sixthNumber = document.getElementById('sixthNumber').value;
+    const bonusNumber = document.getElementById('bonusNumber').value;
+    try {
+      const userLotto = new Lotto([
+        firstNumber,
+        secondNumber,
+        thirdNumber,
+        fourthNumber,
+        fifthNumber,
+        sixthNumber,
+      ]);
+      const { checkedlotto, checkedBonusNumber } = validateBonusNumber(
+        userLotto,
+        bonusNumber,
+      );
     } catch (error) {
       alert(error.message);
     }
   });
 });
 
+document.addEventListener('DOMContentLoaded', () => {
+  const button = document.getElementById('myButton');
+});
 function showLottoList(lottos) {
   const lottoList = document.getElementById('lottoList');
 
