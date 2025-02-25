b7d88de0ea61b0061e23b450cb7847ecf4ae00d9
spoyodevelop <kjy9505@gmail.com>
2025-02-25 18:27:18 +0900

    feat: closeButton에 closeResultModal bind

--- src/step2-index.js
+++ src/step2-index.js
@@ -4,12 +4,13 @@ import {
   handlePurchaseLotto,
   retryGame,
 } from './service/GameService.js';
-import { initPrizeBoard } from './View/LottoView.js';
+import { initPrizeBoard, closeResultModal } from './View/LottoView.js';
 
 function bindEventListeners() {
   elements.purchaseLottoButton.addEventListener('click', handlePurchaseLotto);
   elements.checkResultButton.addEventListener('click', handleCheckResult);
   elements.resetButton.addEventListener('click', retryGame);
+  elements.closeButton.addEventListener('click', closeResultModal);
 }
 
 // TODO, disable button when input is none.
