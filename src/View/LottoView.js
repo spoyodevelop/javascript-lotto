0901394802bad380e3cae3c66fe54cf5307f68d0
spoyodevelop <kjy9505@gmail.com>
2025-02-28 17:55:25 +0900

    refactor: LottoView에서 도메인 관심사 분리

--- src/View/LottoView.js
+++ src/View/LottoView.js
@@ -1,6 +1,9 @@
 import { lottoResults } from '../settings/systemSettings.js';
 import { elements } from './elements.js';
-
+import gameState from '../state/state.js';
+import { bindClipboardCopyEvent } from '../service/GameService.js';
+import showToast from './ToastView.js';
+import { HIDDEN_CLASS, INPUT_IDS } from '../settings/webSettings.js';
 function initPrizeBoard() {
   const idMapping = {
     THREE_MATCH: 'three-match-price',
@@ -17,27 +20,16 @@ function initPrizeBoard() {
     }
   });
 }
+function getUserNumbers() {
+  const userNumbers = INPUT_IDS.map((id) => document.getElementById(id).value);
+  const bonusNumber = document.getElementById('bonus-number').value;
+  return { userNumbers, bonusNumber };
+}
 
 function resetInputs(ids) {
   ids.forEach((id) => {
     const input = document.getElementById(id);
     if (input) input.value = '';
-  });
-}
-function updateWinCount(winCount) {
-  const idMapping = {
-    THREE_MATCH: 'three-match-amount',
-    FOUR_MATCH: 'four-match-amount',
-    FIVE_MATCH: 'five-match-amount',
-    FIVE_MATCH_WITH_BONUS: 'five-match-with-bonus-amount',
-    SIX_MATCH: 'six-match-amount',
-  };
-
-  Object.entries(idMapping).forEach(([key, id]) => {
-    const el = document.getElementById(id);
-    if (el) {
-      el.textContent = `${winCount[key].toLocaleString()}개`;
-    }
   });
 }
 function showLottoList(lottos) {
@@ -97,6 +89,22 @@ function showLottoList(lottos) {
 
   renderItems();
 }
+function updateWinCount(winCount) {
+  const idMapping = {
+    THREE_MATCH: 'three-match-amount',
+    FOUR_MATCH: 'four-match-amount',
+    FIVE_MATCH: 'five-match-amount',
+    FIVE_MATCH_WITH_BONUS: 'five-match-with-bonus-amount',
+    SIX_MATCH: 'six-match-amount',
+  };
+
+  Object.entries(idMapping).forEach(([key, id]) => {
+    const el = document.getElementById(id);
+    if (el) {
+      el.textContent = `${winCount[key].toLocaleString()}개`;
+    }
+  });
+}
 
 function resetLottoList() {
   elements.lottoList.scrollTop = 0;
@@ -115,6 +123,25 @@ function closeResultModal() {
   elements.resultModal.close();
 }
 
+function updatePurchaseUI(ticket) {
+  showLottoList(gameState.lottos);
+  elements.purchaseAmount.textContent = `총 ${ticket}개를 구매하였습니다.`;
+  elements.lottosDiv.classList.remove(HIDDEN_CLASS);
+  elements.checkUserNumberDiv.classList.remove(HIDDEN_CLASS);
+  bindClipboardCopyEvent();
+  showToast(`총 ${ticket}개를 구매하였습니다.`, 'success');
+  elements.purchaseLottoButton.disabled = true;
+  elements.userMoneyInput.disabled = true;
+  resetInputs(['user-money']);
+}
+function resetUI() {
+  elements.checkUserNumberDiv.classList.add(HIDDEN_CLASS);
+  elements.lottosDiv.classList.add(HIDDEN_CLASS);
+  elements.resultModal.close();
+  elements.purchaseLottoButton.disabled = false;
+  elements.userMoneyInput.disabled = false;
+}
+
 export {
   initPrizeBoard,
   resetInputs,
@@ -123,4 +150,7 @@ export {
   resetLottoList,
   showRevenueRate,
   closeResultModal,
+  updatePurchaseUI,
+  getUserNumbers,
+  resetUI,
 };
