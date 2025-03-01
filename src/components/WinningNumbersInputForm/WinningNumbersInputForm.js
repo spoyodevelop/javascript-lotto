50b9552847de0ee723405eb4e1a03440627d0580
swon3210 <swon3210@gmail.com>
2025-03-02 01:06:37 +0900

    feat: WinningNumbers 컴포넌트 구현

---
+++ src/components/WinningNumbersInputForm/WinningNumbersInputForm.js
@@ -0,0 +1,44 @@
+import { calculateLottos } from '../../domain/GameLogic';
+import Lotto from '../../model/Lotto';
+import { HIDDEN_CLASS, INPUT_IDS } from '../../settings/webSettings';
+import validateBonusNumber from '../../Validation/validateBonusNumber';
+import { resetInputs } from '../../View/LottoView';
+import { checkResultButton, checkUserNumberDiv } from './elements';
+
+function getUserNumbers() {
+  const userNumbers = INPUT_IDS.map((id) => document.getElementById(id).value);
+  const bonusNumber = document.getElementById('bonus-number').value;
+  return { userNumbers, bonusNumber };
+}
+
+const WinningNumbersInputForm = {
+  init: ({ onCheckResult }) => {
+    checkResultButton.addEventListener(
+      'click',
+      WinningNumbersInputForm.checkResult,
+    );
+    WinningNumbersInputForm.onCheckResult = onCheckResult;
+  },
+  show: () => {
+    checkUserNumberDiv.classList.remove(HIDDEN_CLASS);
+  },
+  hide: () => {
+    checkUserNumberDiv.classList.add(HIDDEN_CLASS);
+  },
+  reset: () => {
+    resetInputs([...INPUT_IDS, 'bonus-number']);
+  },
+  checkResult: () => {
+    const { userNumbers, bonusNumber } = getUserNumbers();
+    const userLotto = new Lotto(userNumbers);
+    const parsedLotto = validateBonusNumber(userLotto, bonusNumber);
+    const { winCount, revenueRate } = calculateLottos(parsedLotto);
+
+    WinningNumbersInputForm.onCheckResult(winCount, revenueRate);
+  },
+  onCheckResult: () => {
+    console.log('onCheckResult 이 설정되지 않았습니다.');
+  },
+};
+
+export default WinningNumbersInputForm;
---
+++ src/components/WinningNumbersInputForm/elements.js
@@ -0,0 +1,2 @@
+export const checkUserNumberDiv = document.getElementById('check-user-number');
+export const checkResultButton = document.getElementById('check-result');
